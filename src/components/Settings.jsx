6f0407d66bd24dfaa1309ba73c9b75cf966de478
Ernestolop <ernestodaniellopez504@gmail.com>
2023-11-26 14:11:40 -0300

    feat:refactor
    Resolviendo alertas de ESLint

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -6,7 +6,7 @@ const Settings = () => {
   const { maxTimeSeconds, updateMaxTimeSeconds, handleModal, handleAlert } = useSettings();
   const [maxTimeSecondsInput, setMaxTimeSecondsInput] = useState(maxTimeSeconds);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     if (maxTimeSecondsInput < 1 || isNaN(maxTimeSecondsInput)) {
       handleAlert('El tiempo máximo debe ser un número mayor a 0', 'danger');
@@ -21,7 +21,7 @@ const Settings = () => {
     <form className='form' onSubmit={handleSubmit}>
       <div className="form__field">
         <label className='form__label' htmlFor="maxTimeSeconds">Tiempo Máximo Por Miembro del Squad (en segundos)</label>
-        <input className='form__input' type="number" id="maxTimeSeconds" name="maxTimeSeconds" value={maxTimeSecondsInput} onChange={(e) => setMaxTimeSecondsInput(e.target.value)} />
+        <input className='form__input' type="number" id="maxTimeSeconds" name="maxTimeSeconds" value={maxTimeSecondsInput} onChange={e => setMaxTimeSecondsInput(e.target.value)} />
       </div>
       <input type="submit" className="form__submit" value="Guardar Configuraciones" />
     </form>
